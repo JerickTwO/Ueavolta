1554f512fb8a51171bcc4e0012e7c9b25bd02391
JerickTwO <jerickjjtm774@gmail.com>
2024-06-14 15:49:33 -0500

    .

--- src/data/redes_sociales.js
+++ src/data/redes_sociales.js
@@ -1,12 +1,12 @@
 export default [
   {
     nombre: 'LinkedIn',
-    url: 'https://linkedin.com/in/kewinbarboza',
+    url: 'https://linkedin.com/',
     icon: 'linkedin'
   },
   {
     nombre: 'GitHub',
-    url: 'https://github.com/KewinBarboza',
+    url: 'https://github.com/',
     icon: 'github'
   },
 ]
